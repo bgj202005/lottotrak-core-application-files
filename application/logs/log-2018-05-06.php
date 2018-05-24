22db7f1251029515a23eb45ae960581836005e45
Brad Jacobsen <bgj20@hotmail.com>
2018-05-24 13:30:49 -0700

    Testing with Eclipse and will remove.

---
+++ application/logs/log-2018-05-06.php
@@ -0,0 +1,27 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2018-05-06 17:33:13 --> 404 Page Not Found: http://localhost/lottotrak/css/bootstrap.min.css
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2018-05-06 17:33:13 --> 404 Page Not Found: http://localhost/lottotrak/css/animate.css
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2018-05-06 17:33:13 --> 404 Page Not Found: http://localhost/lottotrak/js/jquery-1.7.min.js
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2018-05-06 17:33:13 --> 404 Page Not Found: http://localhost/lottotrak/css/styles.css
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2018-05-06 17:33:13 --> 404 Page Not Found: http://localhost/lottotrak/js/bootstrap.min.js
+ERROR - 2018-05-06 17:33:13 --> 404 Page Not Found: http://localhost/lottotrak/js/superfish.js
+ERROR - 2018-05-06 17:33:14 --> 404 Page Not Found: http://localhost/lottotrak/js/script.js
+ERROR - 2018-05-06 17:33:14 --> 404 Page Not Found: http://localhost/lottotrak/js/FF-cash.js
+ERROR - 2018-05-06 17:33:14 --> 404 Page Not Found: http://localhost/lottotrak/js/jquery.responsivemenu.js
+ERROR - 2018-05-06 17:33:14 --> 404 Page Not Found: http://localhost/lottotrak/js/jquery.hoverIntent.js
+ERROR - 2018-05-06 17:33:14 --> 404 Page Not Found: http://localhost/lottotrak/js/bootstrap.min.js
+ERROR - 2018-05-06 17:33:14 --> 404 Page Not Found: http://localhost/lottotrak/js/jquery-1.7.min.js
+ERROR - 2018-05-06 17:33:14 --> 404 Page Not Found: http://localhost/lottotrak/js/superfish.js
+ERROR - 2018-05-06 17:33:14 --> 404 Page Not Found: http://localhost/lottotrak/js/FF-cash.js
+ERROR - 2018-05-06 17:33:14 --> 404 Page Not Found: http://localhost/lottotrak/js/script.js
+ERROR - 2018-05-06 17:33:14 --> 404 Page Not Found: http://localhost/lottotrak/js/jquery.hoverIntent.js
+ERROR - 2018-05-06 17:33:14 --> 404 Page Not Found: http://localhost/lottotrak/js/jquery.responsivemenu.js
