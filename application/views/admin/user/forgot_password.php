090666733a97bea3e729906858fc8cfba6dc3de0
brad <bgj20@hotmail.com>
2019-11-25 12:45:15 -0800

    Cosmetic Changes to Admin Login / Forgot Password Pages Modals

--- application/views/admin/user/forgot_password.php
+++ application/views/admin/user/forgot_password.php
@@ -1,33 +1,34 @@
-<div class = "model-body">
 	<?php echo validation_errors('<div class="alert alert-warning" role="alert">', '</div>'); 
 	echo $this->session->flashdata('error');
 	echo form_open(); ?>
-	<table class = "table">
-		<tr>
-			<td><span class="glyphicon glyphicon-envelope"></span>&nbsp;Email Address:</td>
-			<td><?php echo form_input('email'); ?></td>
-			<td><?php echo anchor('admin/user/login', 'Back to Login')?></td>
-		</tr>
-		<tr>
-			<td colspan = "3"><div style = "text-align: center; font-weight: bold">So that we know you are human?
-			<br />Enter the series of characters you see below:</div></td>
-		    <td>&nbsp;</td>
-		    <td>&nbsp;</td>  
-		</tr>
-		<tr>
-			<td>&nbsp;</td>
-			<td><?php echo $captcha['image']; ?></td>
-		    <td><?php echo form_button('Reload', 'Refresh', 'class="btn btn-success" onClick="history.go(0)"'); ?></td>
-		</tr>
-		<tr>
-			<td>Type Characters:</td>
-			<td><?php echo form_input('captcha'); ?></td>
-			<td>&nbsp;</td>
-		</tr>
-		<tr>
-    		<td>&nbsp;</td>
-    		<td><?php echo form_submit('submit', 'Continue', 'class="btn btn-primary"'); ?></td>    		
-    		<td>&nbsp;</td>
+	<table class = "table table-borderless">
+		<tbody>	
+			<div class="form-group">
+				<tr>
+					<td scope = "row" style="text-align:left; width:8em;"><i class="fa fa-envelope-o"></i>&nbsp;<?php echo form_label('Email Address:', 'email_l'); ?>
+					<td><?php echo form_input('email', '', 'class="form-control-lg"'); ?></td>
+				</tr>
+				<tr>
+					<td colspan = "2"><div style = "text-align: center; font-weight: bold">So that we know you are human?
+					<br />Enter the series of characters you see below:</div></td>
+					<td>&nbsp;</td>
+				</tr>
+				<tr>
+					<td><?php echo $captcha['image']; ?></td>
+					<td><?php echo form_button('Reload', 'Refresh', 'class="btn btn-success" onClick="history.go(0)"', 'class="form-control-lg"'); ?></td>
+				</tr>
+				<tr>
+					<td scope = "row" style="text-align:left; width:8em;"><i class="fa fa-keyboard-o"></i>Type Characters:</td>
+					<td><?php echo form_input('captcha', '', 'class="form-control-lg"'); ?></td>
+				</tr>
+				<tr>
+					<td>&nbsp;</td>
+					<td><?php echo form_submit('submit', 'Continue', 'class="btn btn-dark btn-primary btn-lg" style="margin-top:1em;"'); ?></td>    		
+				</tr>
+				<tr>
+					<td colspan = "2"><?php echo anchor('admin/user/login', 'Back to Login')?></td>
+					<td>&nbsp;</td>
+				</tr>
+		</tbody>
 	</table>
 <?php echo form_close();?>
-</div>
--- application/views/admin/user/login.php
+++ application/views/admin/user/login.php
@@ -1,29 +1,30 @@
-<div class = "model-body">
 	<?php echo validation_errors('<div class="alert alert-warning" role="alert">', '</div>'); 
 	echo $this->session->flashdata('error');
 	echo (isset($action) ? form_open($action) : form_open()); ?>
-	
-	<table class = "table">
-		<tr>
-			<td><span class="glyphicon glyphicon-user"></span>&nbsp;User Name:</td>
-			<td><?php echo form_input('username'); ?></td>
-		</tr>
-		<tr>
-			<td><span class="glyphicon glyphicon-envelope"></span>&nbsp;Email:</td>
-			<td><?php echo form_input('email'); ?></td>
-		</tr>
-		<tr>
-		     <td>&nbsp;</td>
-		     <td><?php echo anchor('admin/user/forgotpassword', 'Forgot Your Password?')?></td>
-		</tr>
-		<tr>
-			<td><span class="glyphicon glyphicon-lock"></span>&nbsp;Password:</td>
-			<td><?php echo form_password('password'); ?></td>
-		</tr>
-		<tr>
-		<td></td>
-		<td><?php echo form_submit('submit', 'Log in', 'class="btn btn-primary"'); ?></td>
-		</tr>
+	<table class="table table-borderless">
+		<tbody>
+			<div class="form-group">
+				<tr>
+					<td scope = "row" style="text-align:left; width:8em;"><i class="fa fa-user"></i>&nbsp;<?php echo form_label('User Name:', 'username_l');?></td>
+					<td><?php echo form_input('username', '', 'class="form-control-lg"'); ?></td>
+				</tr>
+				<tr>
+					<td scope = "row"><i class="fa fa-envelope-o"></i>&nbsp;<?php echo form_label('Email:', 'email_l'); ?></td>
+					<td><?php echo form_input('email', '', 'class="form-control-lg"'); ?></td>
+				</tr>
+				<tr>
+					<td scope = "row" style="text-align:left; width:8em;"><i class="fa fa-lock"></i>&nbsp;<?php echo form_label('Password:', 'password_l'); ?></td>
+					<td><?php echo form_password('password', '', 'class="form-control-lg'); ?></td>
+				</tr>
+				<tr>
+					<td scope = "row" colspan = 2><?php echo form_submit('submit','Log in','class="btn btn-dark btn-primary btn-lg" style="margin-top:1em;"');?></td>
+					<td>&nbsp;</td>
+				</tr>
+			</div>
+				<tr>
+					<td scope = "row" colspan = 2><?php echo anchor('admin/user/forgotpassword', 'Forgot Your Password?')?></td>
+					<td>&nbsp;</td>
+				</tr>
+		</tbody>
 	</table>
 <?php echo form_close();?>
-</div>
