b86f8df771551783f17f8fd3d47f2e63eb61b442
brad <bgj20@hotmail.com>
2020-05-15 10:56:36 -0700

    New Template Positions / Menu item Hide or Display

---
+++ application/migrations/20200515103000_Add_Template_Position_To_Pages.php
@@ -0,0 +1,23 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+
+class  Migration_Add_template_position_to_pages extends CI_Migration {
+
+	public function up()
+	{
+		$fields  = (array(
+		        'position' => array(
+		                  'type'       => 'INTEGER',
+		                  'constraint' => 10,
+		                  'null'	   => FALSE
+		    )
+		));
+		$this->dbforge->add_column('pages', $fields);
+	}
+
+	public function down()
+	{
+		$this->dbforge->drop_column('pages', 'position');
+	}
+}
