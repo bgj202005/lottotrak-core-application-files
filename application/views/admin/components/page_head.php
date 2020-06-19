8c52d6b3a742b5f6a3642e49ee0d17d8329d3dfe
brad <bgj20@hotmail.com>
2020-06-18 21:47:23 -0700

    Admin - TinyMCE Editor Changes

--- application/views/admin/components/page_head.php
+++ application/views/admin/components/page_head.php
@@ -33,21 +33,10 @@
   tinymce.init({
     selector: '#editarea',
     height: 500,
-    menu: 
-    {
-    file: { title: 'File', items: 'newdocument restoredraft | preview | print ' },
-    edit: { title: 'Edit', items: 'undo redo | cut copy paste | selectall | searchreplace' },
-    view: { title: 'View', items: 'code | visualaid visualchars visualblocks | spellchecker | preview fullscreen' },
-    insert: { title: 'Insert', items: 'image link media template codesample inserttable | charmap emoticons hr | pagebreak nonbreaking anchor toc | insertdatetime' },
-    format: { title: 'Format', items: 'bold italic underline strikethrough superscript subscript codeformat | formats blockformats fontformats fontsizes align | forecolor backcolor | removeformat' },
-    tools: { title: 'Tools', items: 'spellchecker spellcheckerlanguage | code wordcount' },
-    table: { title: 'Table', items: 'inserttable | cell row column | tableprops deletetable' },
-    help: { title: 'Help', items: 'help' }
-    },
     
-    plugins: 'autoresize a11ychecker advcode casechange formatpainter linkchecker autolink lists checklist media mediaembed pageembed permanentpen powerpaste table advtable tinycomments tinymcespellchecker link image code',
-      toolbar: 'anchor a11ycheck addcomment showcomments casechange checklist code formatpainter pageembed permanentpen table | link image code',
-      menubar: "tools",
+    plugins: 'print preview paste importcss searchreplace autolink autosave save directionality code visualblocks visualchars fullscreen image link media template codesample table charmap hr pagebreak nonbreaking anchor toc insertdatetime advlist lists wordcount imagetools textpattern noneditable help charmap quickbars emoticons link image code',
+      toolbar: 'link image code | undo redo | bold italic underline strikethrough | fontselect fontsizeselect formatselect | anchor | alignleft aligncenter alignright alignjustify | outdent indent |  numlist bullist | forecolor backcolor removeformat | pagebreak | charmap emoticons | fullscreen preview save print',
+      menubar: 'file edit view insert format tools table help',
       relative_urls: false,
       toolbar_mode: 'floating',
       tinycomments_mode: 'embedded',
