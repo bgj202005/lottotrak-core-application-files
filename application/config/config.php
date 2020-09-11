b724bf38d5abc769a6a83c6c1202f192d486902a
brad <bgj20@hotmail.com>
2020-09-11 13:30:30 -0700

    Deprecated changes for _autoload to spl_autoload

--- application/config/config.php
+++ application/config/config.php
@@ -1,14 +1,14 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-function __autoload($classname)
+spl_autoload_register(function ($classname)
 { if (strpos($classname, 'CI_') !== 0) {
 	$file = APPPATH.'libraries/'.$classname.'.php';
 	if (file_exists($file) && is_file($file)){
 		@include_once($file);
 	}
   }
-}
+});
 /*
 |--------------------------------------------------------------------------
 | Base Site URL
