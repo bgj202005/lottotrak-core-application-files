f9c32a87a2d113c8dd62bfe416e1d3fe6f1d947e
brad <bgj20@hotmail.com>
2021-06-24 09:05:56 -0700

    Updated Page Controller

--- application/controllers/page.php
+++ application/controllers/page.php
@@ -1,28 +1,65 @@
 <?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
 class Page extends Frontend_Controller {
 	
 	function __construct() {
 		parent::__construct();
-		$this->load->model('page_m');
+		$this->load->model('maintenance_m'); 
 	}
 
-	public function index() {
-		$slug = (is_null($this->uri->segment(1)) ? array('slug' => (string) 'home') :  array('slug' => (string) $this->uri->segment(1)));
-		$this->data['page'] = $this->page_m->get_by($slug, TRUE);
-		count($this->data['page']) || show_404(current_url());
-		 
-		//echo '<pre>'. $this->db->last_query().'</pre>';
-		$this->data['recent_news'] = $this->article_m->get_recent();
-		
-		$method = '_'.$this->data['page']->template;
-		
-		if (method_exists($this, $method)) {
-			$this->$method();
-		} else {
-			log_message('error', 'Could not load template '.$method.' in file ' . __FILE__. ' at line '. __LINE__);
-		} 
-		$this->data['subview'] = $this->data['page']->template;
-		$this->load->view('_main_layout', $this->data);
+	public function index() 
+	{
+		$this->data['maintenance'] = $this->maintenance_m->maintenance_check();
+		if(!$this->data['maintenance']) 
+		{
+			$slug = (is_null($this->uri->segment(1)) ? array('slug' => (string) 'home') :  array('slug' => (string) $this->uri->segment(1)));
+			$this->data['page'] = $this->page_m->get_by($slug, TRUE);
+			if(is_null($this->data['page']))
+			{
+				$this->error_404($slug); // Related Page not found
+			}
+			else
+			{
+				$this->data['page']->body = stripslashes($this->data['page']->body); // Remove the slashes from the database.
+				if (!is_null($this->data['page']->raw)) $this->data['page']->raw = stripslashes($this->data['page']->raw); // Remove the slashes from the database.
+				if (!is_null($this->data['page']->description)) $this->data['page']->description = stripslashes($this->data['page']->description);
+				/* (!empty($this->data['page'])) || show_404(current_url()); */ // Depreciated in PHP 7.2 count($this->data['page'])
+					if($this->data['page']->template=='homepage')  // Include Bottom Left & Bottom Right Positions
+					{
+						$this->data['page_bottom_left'] = $this->page_m->home_pages('bottom_left');
+						$this->data['page_bottom_left']->body = stripslashes($this->data['page_bottom_left']->body); // Remove the slashes from the database.
+						$this->data['page_bottom_right'] = $this->page_m->home_pages('bottom_right');
+						$this->data['page_bottom_right']->body = stripslashes($this->data['page_bottom_right']->body); // Remove the slashes from the database.
+					} 
+					$this->data['sidebar_top'] = $this->page_m->side_bar('top_section');
+					$this->data['sidebar_top']->body = stripslashes($this->data['sidebar_top']->body); // Remove the slashes from the database.
+					$this->data['sidebar_middle'] = $this->page_m->home_pages('middle_section');
+					$this->data['sidebar_middle']->body = stripslashes($this->data['sidebar_middle']->body); // Remove the slashes from the database.
+					$this->data['sidebar_bottom'] = $this->page_m->side_bar('bottom_section');
+					$this->data['sidebar_bottom']->body = stripslashes($this->data['sidebar_bottom']->body); // Remove the slashes from the database.
+
+					$this->data['recent_news'] = $this->article_m->get_recent();
+				
+				$method = '_'.$this->data['page']->template;
+				
+				if (method_exists($this, $method)) {
+					$this->$method();
+				} else 
+				{
+					log_message('error', 'Could not load template '.$method.' in file ' . __FILE__. ' at line '. __LINE__);
+				} 
+				add_meta_title($this->data['page']->title);
+				add_meta_description($this->data['page']->description);
+				add_meta_canonical($this->data['page']->canonical);
+				$this->data['subview'] = $this->data['page']->template;
+				$this->load->view('_main_layout', $this->data);
+			}
+		}
+		else
+		{
+			$this->offline(); // Present the entire frontend offline with the page offline template. Give a maintenance message
+		}
 	}
 	
 	private function _page() {
@@ -104,7 +141,7 @@ class Page extends Frontend_Controller {
 				} else { 
 				// Make sure the user does not exist and the email does not exist.
 					$error = validation_errors();
-					echo json_encode(['js-reg-error'=>$errors]);
+					echo json_encode(['js-reg-error'=>$error]);
 					/* $this->form_validation->set_message('signup_error', '<div class="alert alert-danger signup_error">This Username Already Exists.</div>'); */
 				}
 				
@@ -169,12 +206,11 @@ class Page extends Frontend_Controller {
 		}
 	}
 	
-	public function _unique_username()
+	public function _unique_username($id)
 	{
 		// Do Not validate if email already exists
 		// Unless it's the email for the current user	
 		
-		//dump($id); exit(1);	
 		$this->db->where('username', $this->input->post('username'));
 			!$id || $this->db->where('id !=', $id);
 			$user = $this->user_m->get();
@@ -186,7 +222,7 @@ class Page extends Frontend_Controller {
 	return TRUE;
 	}
 	
-	public function _unique_email()
+	public function _unique_email($id)
 	{
 		// Do Not validate if email already exists
 		// Unless it's the email for the current user
@@ -201,9 +237,49 @@ class Page extends Frontend_Controller {
 		}
 		return TRUE;
 	}
+
+	/**
+	 * Error 404 Pages
+	 * @param       $keyword	Same as Slug on the page but it is not a slug that currently exists
+	 * @return      none
+ 	*/
+	public function error_404($keyword)
+	{
+	   $this->output->set_status_header('404');
+	   $this->data['sidebar_top'] = $this->page_m->side_bar('top_section');
+	   $this->data['sidebar_top']->body = stripslashes($this->data['sidebar_top']->body); // Remove the slashes from the database.
+ 	   $this->data['sidebar_middle'] = $this->page_m->home_pages('middle_section');
+	   $this->data['sidebar_middle']->body = stripslashes($this->data['sidebar_middle']->body); // Remove the slashes from the database.
+	   $this->data['sidebar_bottom'] = $this->page_m->side_bar('bottom_section');
+	   $this->data['sidebar_bottom']->body = stripslashes($this->data['sidebar_bottom']->body); // Remove the slashes from the database.
+
+	   $this->data['recent_news'] = $this->article_m->get_recent();
+	   $this->data['slug'] = $keyword['slug']; //keyword / slug
+       $this->data['subview'] = 'errors/error_404';
+	   add_meta_title($this->data['slug'].' not found');
+	   add_meta_description('Check the URL of the page and try again.  This page does not exist.');
+	   add_meta_canonical(0);
+	   add_meta_noindex();
+	   $this->load->view('_main_layout', $this->data);
+    }
 	
-	function DashBoard() {
-		exit ('Stop');
-	}
-	
+	/**
+	 * Sets the Website Offline Message to the Frontend
+	 * 
+	 * @param       none
+	 * @return      none
+	 */
+	public function offline()
+	{
+	   $this->data['sidebar_top'] = $this->page_m->side_bar('top_section');
+	   $this->data['sidebar_top']->body = stripslashes($this->data['sidebar_top']->body); // Remove the slashes from the database.
+ 	   $this->data['sidebar_middle'] = $this->page_m->home_pages('middle_section');
+	   $this->data['sidebar_middle']->body = stripslashes($this->data['sidebar_middle']->body); // Remove the slashes from the database.
+	   $this->data['sidebar_bottom'] = $this->page_m->side_bar('bottom_section');
+	   $this->data['sidebar_bottom']->body = stripslashes($this->data['sidebar_bottom']->body); // Remove the slashes from the database.
+
+	   $this->data['recent_news'] = $this->article_m->get_recent();
+       $this->data['subview'] = 'offline';
+	   $this->load->view('_main_layout', $this->data);
+    }
 }
