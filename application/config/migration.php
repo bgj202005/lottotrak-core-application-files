9244b4e68ef0b5ac32b73cd7879c5e4b9bdb61fc
brad <bgj20@hotmail.com>
2021-07-29 11:32:22 -0700

    Added a nonfollower table in db

--- application/config/migration.php
+++ application/config/migration.php
@@ -69,7 +69,7 @@ $config['migration_auto_latest'] = FALSE;
 | be upgraded / downgraded to.
 |
 */
-$config['migration_version'] = 20210715102000;
+$config['migration_version'] = 20210729113000;
 
 /*
 |--------------------------------------------------------------------------
---
+++ application/migrations/20210729113000_create_lottery_nonfollowers.php
@@ -0,0 +1,46 @@
+<?php
+
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Migration_Create_Lottery_Nonfollowers extends CI_Migration {
+
+        public function up()
+        {
+        	$this->dbforge->add_key('id', TRUE);
+        	$this->dbforge->add_field(array(
+                        'id' => array(
+                            'type' => 'INT',
+                            'constraint' => 11,
+                            'unsigned' => TRUE,
+                            'auto_increment' => TRUE
+                        ),
+                        'range' => array(
+                            'type' => 'INT',
+                            'constraint' => '11',
+                            'unsigned' => TRUE
+                        ),
+                        'lottery_nonfollowers' => array(
+        	                'type' => 'VARCHAR',
+        	                'constraint' => '2000'
+                        ),
+                        'draw_id' => array(
+                            'type' => 'INT',
+                            'constraint' => '11',
+                            'unsigned' => TRUE
+                    ),
+                        'lottery_id' => array(
+                            'type' => 'INT',
+                            'constraint' => '11',
+                            'unsigned' => TRUE
+                    )
+                ));
+                $this->dbforge->add_key('draw_id'); // Non-primary and foreign key
+                $this->dbforge->add_key('lottery_id'); // Non-primary and foreign key
+                $this->dbforge->create_table('lottery_nonfollowers');
+        }
+
+        public function down()
+        {
+                $this->dbforge->drop_table('lottery_nonfollowers');
+        }
+}
