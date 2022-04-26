16d72bdbc2005ba945cd23c211a9100d6f372015
bgj202005 <bgj20@hotmail.com>
2022-04-26 09:17:01 -0700

    H-W-C History Stats setup in DB

--- application/config/migration.php
+++ application/config/migration.php
@@ -69,7 +69,7 @@ $config['migration_auto_latest'] = FALSE;
 | be upgraded / downgraded to.
 |
 */
-$config['migration_version'] = 20220217072100;
+$config['migration_version'] = 20220426091500;
 
 /*
 |--------------------------------------------------------------------------
---
+++ application/migrations/20220426091500_create_lottery_h_w_c_stats.php
@@ -0,0 +1,70 @@
+<?php
+
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Migration_Create_Lottery_h_w_c_stats extends CI_Migration {
+
+        public function up()
+        {
+        	$this->dbforge->add_key('id', TRUE);
+        	$this->dbforge->add_field(array(
+                        'id' => array(
+                            'type' => 'INT',
+                            'constraint' => 11,
+                            'unsigned' => TRUE,
+                            'auto_increment' => TRUE
+                        ),
+                        'range' => array(
+                            'type' => 'INT',
+                            'constraint' => '11',
+                            'unsigned' => TRUE
+                        ),
+                        'h_w_c_range' => array(
+        	                'type' => 'VARCHAR',
+        	                'constraint' => '500'
+                        ),
+                        'h_w_c_last_1' => array(
+        	                'type' => 'VARCHAR',
+        	                'constraint' => '5'
+                        ),
+                        'h_w_c_last_100' => array(
+        	                'type' => 'VARCHAR',
+        	                'constraint' => '500'
+                        ),
+                        'h_w_c_last_200' => array(
+        	                'type' => 'VARCHAR',
+        	                'constraint' => '500'
+                        ),
+                        'draw_id' => array(
+                            'type' => 'INT',
+                            'constraint' => '11',
+                            'unsigned' => TRUE
+                        ),
+                        'lottery_id' => array(
+                             'type' => 'INT',
+                             'constraint' => '11',
+                             'unsigned' => TRUE
+                        ),
+                        'extra_included' => array(
+                            'type' => 'INT',
+                            'constraint' => 1,
+                            'unsigned' => FALSE,
+                            'default' => 0
+                        ),
+                        'extra_draws' => array(
+                            'type' => 'INT',
+                            'constraint' => 1,
+                            'unsigned' => FALSE,
+                            'default' => 0
+                        )
+                ));
+                $this->dbforge->add_key('draw_id'); // Non-primary and foreign key
+                $this->dbforge->add_key('lottery_id'); // Non-primary and foreign key
+                $this->dbforge->create_table('lottery_h_w_c_stats');
+        }
+
+        public function down()
+        {
+                $this->dbforge->drop_table('lottery_h_w_c_stats');
+        }
+}
