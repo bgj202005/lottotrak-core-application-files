ebadbfa1496bb2a3cbd728bf65d8f165763799d5
bgj202005 <bgj20@hotmail.com>
2024-04-16 21:02:49 -0700

    update: lottery predictions view swap between predictions and prize history

--- application/views/admin/dashboard/predictions/index.php
+++ application/views/admin/dashboard/predictions/index.php
@@ -16,8 +16,8 @@
 				<th>Combinations</th>
 				<th>Generate Wheel</th>
 				<th>Files</th>
-				<th>Prize History</th>
 				<th>Predictions</th>
+				<th>Prize History</th>
 			</tr>
 		</thead>
 		<tbody>
@@ -49,8 +49,8 @@
 	    <td style = "text-align:center;"><?php echo $predictions->btn_calculate('admin/predictions/combinations/'.$lottery->id); ?></td>
 		<td style = "text-align:center;"><?php echo $predictions->btn_generate('admin/predictions/generate/'.$lottery->id, $predictions->active($lottery->id)); ?></td>
 		<td style = "text-align:center;"><?php echo $predictions->btn_files('admin/predictions/files/'.$lottery->id, $predictions->active($lottery->id)); ?></td>
-		<td style = "text-align:center;"><?php echo $predictions->btn_wins('admin/predictions/history/'.$lottery->id); ?></td>
 		<td style = "text-align:center;"><?php echo $predictions->btn_predicts('admin/predictions/predicts/'.$lottery->id); ?></td>
+		<td style = "text-align:center;"><?php echo $predictions->btn_wins('admin/predictions/history/'.$lottery->id); ?></td>
 	</tr>
 	<?php endforeach; ?> 
 	
