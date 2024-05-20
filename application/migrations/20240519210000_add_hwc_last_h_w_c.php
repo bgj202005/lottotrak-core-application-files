08317b84879dcf0bc2fe760ce06dd9289ca0a948
bgj202005 <bgj20@hotmail.com>
2024-05-20 07:24:01 -0700

    Added: h_w_c hots_last, warms_last, colds_last fields (table:lottery_h_w_c) and position_last (table:lottery_h_w_c_stats).  Added to the current H-W-C Wins view (Lottery Actual Win History).

---
+++ application/migrations/20240519210000_add_hwc_last_h_w_c.php
@@ -0,0 +1,36 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Migration_Add_Hwc_last_H_w_c extends CI_Migration {
+    
+    public function up()
+    {
+        $fields = (array(
+            'hots_last' => array(
+        	     'type' => 'VARCHAR',
+        	    'constraint' => '1000',
+                'null' => false, 
+                'after' => 'hots'
+             ),
+            'warms_last' => array(
+        	   'type' => 'VARCHAR',
+        	    'constraint' => '1000',
+                'null' => false, 
+                'after' => 'warms'
+            ),
+            'colds_last' => array(
+                'type' => 'VARCHAR',
+                'constraint' => '1000',
+                'null' => false, 
+                'after' => 'colds'
+            ),
+        ));
+        $this->dbforge->add_column('lottery_h_w_c', $fields);
+    }
+    public function down()
+    {
+        $this->dbforge->drop_column('lottery_h_w_c', 'hots_last');
+        $this->dbforge->drop_column('lottery_h_w_c', 'warms_last');
+        $this->dbforge->drop_column('lottery_h_w_c', 'colds_last');
+    }
+}
---
+++ application/migrations/20240519210500_add_position_last_h_w_c_stats.php
@@ -0,0 +1,22 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Migration_Add_Position_Last_H_w_c_stats extends CI_Migration {
+    
+    public function up()
+    {
+        $fields = (array(
+            'position_last' => array(
+        	    'type' => 'VARCHAR',
+        	    'constraint' => '3000',
+                'null' => false, 
+                'after' => 'position'
+            )
+        ));
+        $this->dbforge->add_column('lottery_h_w_c_stats', $fields);
+    }
+    public function down()
+    {
+        $this->dbforge->drop_column('lottery_h_w_c_stats', 'position_last');
+    }
+}
