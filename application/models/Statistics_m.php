b350f60e28dc5b85ed122305bc275ab3cc9266ab
bgj202005 <bgj20@hotmail.com>
2024-07-16 06:26:03 -0700

    Update: position_nolasts method, if null copy $position value to $position_last (the previous draw positions)

--- application/models/Statistics_m.php
+++ application/models/Statistics_m.php
@@ -3168,12 +3168,12 @@ class Statistics_m extends MY_Model
 	{
 		$query = $this->db->query('SELECT `position_last` FROM `lottery_h_w_c_stats` WHERE `lottery_id` ='.$id.';');
 		// If any of the fields are empty, copy the corresponding values from hots, warms, and colds to hots_last, warms_last, and colds_last, respectively
-		$position_last = $query->row();
-		if(is_null($position_last))
+		$row = $query->row();
+		if(empty($row->position_last))
 		{
-			$position_last = $position;
+			$row->position_last = $position;
 		}
-	return $position_last;
+	return $row->position_last;
 	}
 	/**
 	 * if table-lottery_h_w_c returns Null for position_last, no previous h_w_c has been saved to DB 
