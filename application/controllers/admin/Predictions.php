da22e7b61cf4d6df2fe62da36653260201581465
bgj202005 <bgj20@hotmail.com>
2025-08-11 15:08:25 -0700

    added: reset method required combination_files to be listed same as the combination method. Enpty combination files are eliminated from the dropdown until combinations generated

--- application/controllers/admin/Predictions.php
+++ application/controllers/admin/Predictions.php
@@ -636,7 +636,7 @@ class Predictions extends Admin_Controller {
 				$numB = intval(preg_replace('/\D/', '', $b['file_name']));
 				return $numA - $numB;
 			});
-	}
+		}
 	
 	// Fetch H-W-C, Followers, and Friends data
 	$this->data['h_w_c'] = $this->lottery_data_m->get_h_w_c($id);
@@ -1161,7 +1161,27 @@ class Predictions extends Admin_Controller {
 	$this->data['state_prov_code'] = $this->lottery_data_m->get_lottery_state_prov($id);
 	// Fetch combination files for the lottery
 	$this->data['combination_files'] = $this->predictions_m->get_combination_files($id);
-	
+	// Before passing $combination_files to the view
+	if (!empty($this->data['combination_files'])) {
+			// Transform the combination files to include id|filename in value
+		foreach ($this->data['combination_files'] as $index => &$file) {
+			$file_path = $this->combination_files_m->full_path($file['file_name']);
+			$file_content = file_get_contents($file_path); // Read file content
+			if (!empty(trim($file_content))) {
+				$file['value'] = $file['id'] . '|' . $file['file_name']; // e.g., "246|060828"
+				$file['display'] = $file['file_name']; // Keep original filename for display
+			}
+			else {
+				unset($this->data['combination_files'][$index]); // Remove file with no content
+			}
+		}
+		usort($this->data['combination_files'], function($a, $b) {
+			// Extract the number part from the file name (assuming format like "06120500.txt")
+			$numA = intval(preg_replace('/\D/', '', $a['file_name']));
+			$numB = intval(preg_replace('/\D/', '', $b['file_name']));
+			return $numA - $numB;
+		});
+	}
 	// Sort combination files numerically like in the futures method
 	if (!empty($this->data['combination_files'])) {
 		usort($this->data['combination_files'], function($a, $b) {
