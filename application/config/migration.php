56468b4ef17d3c9a26ccbc9cdf286262390a3bb9
bgj202005 <bgj20@hotmail.com>
2025-10-24 11:33:08 -0700

    Added: migration for numbers field in the lottery_combination_filters table (300) varchars to store the generated numbers from the prediction futures view. Saved Filtered Tickets Generated Numbers.

--- application/config/migration.php
+++ application/config/migration.php
@@ -69,7 +69,7 @@ $config['migration_auto_latest'] = FALSE;
 | be upgraded / downgraded to.
 |
 */
-$config['migration_version'] = 20250927210000;
+$config['migration_version'] = 20251024113000;
 
 /*
 |--------------------------------------------------------------------------
