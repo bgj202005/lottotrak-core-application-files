604aac7b555bff562117d7682110f1324e019978
bgj202005 <bgj20@hotmail.com>
2026-08-03 10:40:32 -0700

    Revert "Corrections: Not showing the actual count of Draws. Replaced $draw_count with total_winners array element for H-W-C, Followers and H-W-C + Followers views"
    This reverts commit a6701b401d042244f7586231c6313d45c3ab1138.

--- application/views/admin/dashboard/history/h_w_c.php
+++ application/views/admin/dashboard/history/h_w_c.php
@@ -2108,7 +2108,7 @@
 								<strong>Start Date:</strong> <?php echo $hwc_win_stats['startdate'] ? date('l F j, Y', strtotime($hwc_win_stats['startdate'])) : date('l F j, Y', strtotime($lottery->next_draw_date)); ?>
 								&nbsp;&nbsp;|
 								<?php if($hwc_win_stats['lastdate']): ?>
-									<strong>Last Draw Date:</strong> <?php echo date('l F j, Y', strtotime($hwc_win_stats['lastdate'])); ?> (<?php echo $hwc_win_stats['total_winners']; ?> draw<?php echo $hwc_win_stats['total_winners'] != 1 ? 's' : ''; ?>)
+									<strong>Last Draw Date:</strong> <?php echo date('l F j, Y', strtotime($hwc_win_stats['lastdate'])); ?> (<?php echo $draw_count; ?> draw<?php echo $draw_count != 1 ? 's' : ''; ?>)
 								<?php else: ?>
 									<strong>Last Draw Date:</strong> None yet
 								<?php endif; ?>
